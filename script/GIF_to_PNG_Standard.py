7a162a33f104698de5058698e70a7aeb0a46c949
seanpavlak <seanpavlak@gmail.com>
2021-06-14 15:35:30 -0400

    script and images added

---
+++ script/GIF_to_PNG_Standard.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+from PIL import Image, ImageDraw, ImageFont #dynamic import
+import glob
+
+
+# In[33]:
+
+
+# images = glob.glob('..//images//luma//*.gif')
+images = glob.glob('..//images//standard//*.gif')
+
+
+# In[34]:
+
+
+for image in images:
+    gif = image.split(".gif")
+    img = Image.open(image)
+    img.save(gif[0]+".png",'png', optimize=True, quality=100)
+
+
+# In[35]:
+
+
+images
+
+
+# In[ ]:
+
+
+
+
